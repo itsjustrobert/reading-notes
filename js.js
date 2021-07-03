ca79de8f4e7c5a305ae9a2a3823719a58890fab1
itsjustrobert <robertlove99999@gmail.com>
2021-07-02 21:01:23 -0500

    some notes

---
+++ js.js
@@ -0,0 +1,124 @@
+// console.console.log(2 * 7);
+// console.log(8 / 2);
+
+// // modulo returns the remainder of a division operation
+
+// console.log( 5 % 4);
+// // the remainder would be one
+
+// comparison operators
+// <,>,<=,>=,==,===, !==, !=
+
+// var user_age = prompt("whats your age");
+
+// console.log(user_age > 15);
+
+/* if(user_age < 15){alert(
+    "you need parental permission"
+)}*/
+// if(user_age > 15){alert(
+//     "you are allowerd"
+// )
+
+// }
+// if(user_age === 15){alert
+//     ("you are 15")}
+
+
+// // normal equal == only checks the value
+// // strictly equal === checks value and type
+// // strictly not equal !== checks if type and value are DIFFERENT
+// // not equal == checks if the value of two things is false
+
+// // nested if statements
+// if(user_age < 12){if(user_age > 5){alert("you are between 6 and 11")}}
+
+// else{ alert( "you are younger than 12 and younger than 6")
+
+
+// }
+// else{
+
+// }
+// watch youtube about it!!!
+
+
+// logical operators
+// && (and), || (or), !(not)
+ 
+// var sleepy = true;
+// var hungry = true;
+// if(sleepy && hungry){alert
+//     ("you are sleepy and hungry");
+// }
+
+// if(sleepy || hungry){
+//     alert("you are sleepy or hungry");
+
+// }
+
+// if(!sleepy){
+//     alert("you are not sleepy");
+// }
+// if(!hungry){
+//     alert("you are not hungry");
+// }
+// //  var is global
+// // let is local
+
+// for(let x = 0 ; x < 10; x++){
+//     console.log(x)
+// }
+
+// for loops
+// let starCount = prompt("how many stars would u give us")
+
+// for(let i = 1; i = starCount; i++){
+//     console.log(i);
+// }
+
+// var x = 5;
+// console.log(--x);
+// console.log("my value for x is : " + x);
+
+// a functions that takes in a value, multiply it by 2 then return it
+
+// function double(x){
+//     console.log(x * 2);
+//     let doub = x * 2;
+//     return x ;
+// }
+// double(5);
+
+// // prompt the user to enter page, continue asking
+// // until they do
+// function paper(){
+//     let question = prompt("what something u write on");
+//     while(question != "paper"){question = prompt("what something u write on");
+
+//         alert("please ebter paper");
+//         question = prompt("what something u write on");
+//   youtubbeeee!!!!!!!!
+
+// ask the user to enter a number 1 and 10 and then teel them if they gessed correctly
+
+
+var number = prompt("guess a number between 1 and 10");
+var correctNumber = 9;
+if(correctNumber == number){
+    alert("you guessed correctly")
+    if(number > correctNumber){
+        alert("your number is too high");
+        if(number < correctNumber){
+            alert('your number is too low');
+        }
+    }}
+
+    // loop for game
+
+    let stopGuessing = false;
+
+    while( !stopGuessing){
+        stopGuessing = number();
+    }
+    
